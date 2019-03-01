1af4168ba62ed3f80985c638040f805a56ad7ca3
tiffanywgchan <tiffany.chan@queensu.ca>
2019-03-01 17:24:43 -0500

    separated header/footer and edited permission settings

---
+++ headerUser.php
@@ -0,0 +1,74 @@
+<!-- Created on January 28th, 2019. --> 
+<!-- Written by: Tiffany C. -->
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="Queen's Chemistry Department's Repair Ticketing System">
+    <meta name="author" content="">
+
+    <title>QU Chemistry Repair</title>
+
+    <!--Bootstrap core CSS-->
+    <link rel="stylesheet" type="text/css" href="./public/bootstrap-4.0.0-dist/css/bootstrap.min.css"> 
+      
+    <!--Bootstrap core JavaScript-->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <script src="./public/bootstrap-4.0.0-dist/js/bootstrap.min.js"></script>
+    
+     <!--FontAwesome stylesheet(s)-->
+    <link rel="stylesheet" href="public/font/fontawesome-5.7.1/css/all.css">
+      
+    <!--Open Sans font-->
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+      
+    <!--Custom stylesheet(s)-->
+    <link href="public/css/style.css" rel="stylesheet">
+
+    <!--favicon image-->
+    <link rel="shortcut icon" href="public/images/favicon.ico">
+  </head>
+
+  <body id="page-top">
+
+    <!-- Navigation -->
+    <nav class="navbar navbar-expand-md navbar-dark" id="mainNav">
+            
+        <a class="navbar-brand" href="myTickets.html">
+            <img src="public/images/ChemistryRepairTicketLogo.png" alt="Queen's Department of Chemistry Logo" alt="logo" height="115" width="280">
+        </a>
+        
+        <div class="row">
+        <button class="navbar-toggler ml-auto" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+            </div>
+       
+
+        <div class="collapse navbar-collapse" id="navbarResponsive">
+            <ul class="nav navbar-nav ml-auto">
+                <li class="nav-item active">
+                    <!--change the href's to corresponding pages-->
+                    <a class="nav-link" href="myTickets.html">My Tickets</a>
+                </li>
+            <li class="nav-item">
+                <a class="nav-link" href="createTicket.html">Create Ticket</a>
+                </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#howTo.html">How To</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#profile.html">Profile</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+      
+      
+    <!-- Include Footer -->
+    <?php include('footer.php');?>
+      
+  </body>
+</html>
