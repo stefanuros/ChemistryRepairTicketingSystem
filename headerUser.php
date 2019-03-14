18bf2c3384c418568aa47d9692263d60d40edc61
tiffanywgchan <tiffany.chan@queensu.ca>
2019-03-13 23:49:48 -0400

    adjusted css position to unstick footer

--- headerUser.php
+++ headerUser.php
@@ -1,37 +1,6 @@
 <!-- Created on January 28th, 2019. --> 
 <!-- Written by: Tiffany C. -->
-<!DOCTYPE html>
-<html lang="en">
-
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="Queen's Chemistry Department's Repair Ticketing System">
-    <meta name="author" content="">
-
-    <title>QU Chemistry Repair</title>
-
-    <!--Bootstrap core CSS-->
-    <link rel="stylesheet" type="text/css" href="./public/bootstrap-4.0.0-dist/css/bootstrap.min.css"> 
-      
-    <!--Bootstrap core JavaScript-->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-    <script src="./public/bootstrap-4.0.0-dist/js/bootstrap.min.js"></script>
-    
-     <!--FontAwesome stylesheet(s)-->
-    <link rel="stylesheet" href="public/font/fontawesome-5.7.1/css/all.css">
-      
-    <!--Open Sans font-->
-    <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
-      
-    <!--Custom stylesheet(s)-->
-    <link href="public/css/style.css" rel="stylesheet">
-
-    <!--favicon image-->
-    <link rel="shortcut icon" href="public/images/favicon.ico">
-  </head>
-
-  <body id="page-top">
+<!-- header for User file to include in other php files -->
 
     <!-- Navigation -->
     <nav class="navbar navbar-expand-md navbar-dark" id="mainNav">
@@ -65,6 +34,3 @@
             </ul>
         </div>
     </nav>
-      
-  </body>
-</html>
--- howTo.php
+++ howTo.php
@@ -4,38 +4,69 @@
 <!DOCTYPE html>
 <html lang="en">
 
+<html lang="en">
+
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="Queen's Chemistry Department's Repair Ticketing System">
     <meta name="author" content="">
-      
+
+    <title>QU Chemistry Repair</title>
+
     <!--Bootstrap core CSS-->
-    <link rel="stylesheet" type="text/css" href="./public/bootstrap-4.0.0-dist/css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="./public/bootstrap-4.0.0-dist/css/bootstrap.min.css"> 
       
     <!--Bootstrap core JavaScript-->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
     <script src="./public/bootstrap-4.0.0-dist/js/bootstrap.min.js"></script>
+    
+     <!--FontAwesome stylesheet(s)-->
+    <link rel="stylesheet" href="public/font/fontawesome-5.7.1/css/all.css">
       
+    <!--Open Sans font-->
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+      
+    <!--Custom stylesheet(s)-->
+    <link href="public/css/style.css" rel="stylesheet">
+    <link href="public/css/howTo.css" rel="stylesheet">
+
     <!--favicon image-->
-    <link rel="shortcut icon" href="./public/images/favicon.ico">
+    <link rel="shortcut icon" href="public/images/favicon.ico">
   </head>
 
   <body>
 
     <!-- Loads the user's version of the header -->
     <!-- howTo page is only available on user's version -->
-      <?php include("headerUser.php");?>
-      <div class="col">
-          <div class="row-xs">
-          
-          </div>
-          <div class="row-xs">
-          
-          </div>
-          <div class="row-xs">
-          
-          </div>
+    <?php include("headerUser.php");?>
+    
+    <div class="container">
+        <h3>Getting Started</h3>
+    </div>  
+      
+    <!-- Boxes listing getting started steps -->
+    <div class="container">
+          <div class="row justify-content-center">
+            <div class="square">
+                
+            </div>
+          </div> 
+         <div class="row justify-content-center">
+            <div class="square">
+            
+            </div>
+         </div>
+         <div class="row justify-content-center">
+            <div class="square">
+            
+            </div>
+         </div>
+         <div class="row justify-content-center">
+            <div class="square">
+            
+            </div>
+         </div>
       </div>
       
     <!-- Loads the footer -->
